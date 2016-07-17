f307bfacceaf6e8ba76e19e5a77db4f8e1f33306
Sunjay Varma <varma.sunjay@gmail.com>
2016-07-17 00:38:10 -0400

    Basic kiss3D setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# voxel-editor
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate kiss3d;
+extern crate nalgebra as na;
+
+use na::Vector3;
+use kiss3d::window::Window;
+use kiss3d::light::Light;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new("Kiss3d: cube");
+    let mut c      = window.add_cube(1.0, 1.0, 1.0);
+
+    c.set_color(1.0, 0.0, 0.0);
+
+    window.set_light(Light::StickToCamera);
+
+    while window.render() {
+        c.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.014, 0.0));
+    }
 }
+
