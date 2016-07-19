a31c58b73d677beec0c4c419c89465c6cb09fdbc
Sunjay Varma <varma.sunjay@gmail.com>
2016-07-19 01:30:13 -0400

    Figured out how to stop kiss3D auto event handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
+extern crate glfw;
 extern crate kiss3d;
 extern crate nalgebra as na;
 
-use na::Vector3;
+use glfw::WindowEvent;
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
-    let mut c      = window.add_cube(1.0, 1.0, 1.0);
+    let mut window = Window::new("Voxel Editor");
+    let mut c = window.add_cube(0.5, 0.5, 0.5);
 
     c.set_color(1.0, 0.0, 0.0);
 
     window.set_light(Light::StickToCamera);
 
-    while window.render() {
-        c.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.014, 0.0));
+    while !window.should_close() {
+        for mut event in window.events().iter() {
+            match event.value {
+                WindowEvent::Scroll(x, y) => println!("{} {}", x, y),
+                _ => {}
+            }
+            // do not let any events get handled by the engine
+            event.inhibited = true;
+        }
+
+        window.render();
     }
 }
 
