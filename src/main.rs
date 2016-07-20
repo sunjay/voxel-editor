0407750393f6a7fb37aff8dae52bbdfd51bef61d
Sunjay Varma <varma.sunjay@gmail.com>
2016-07-20 02:14:42 -0400

    Getting the status of ctrl + alt + shift

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 
 use na::Point3;
-use glfw::WindowEvent;
+use glfw::{WindowEvent, Key, Action};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 use kiss3d::camera::{Camera, ArcBall};
@@ -16,7 +16,18 @@ fn main() {
 
     window.set_light(Light::Absolute(na::Point3::new(0.0, 10.0, 10.0)));
 
+    let mut ctrl = false;
+    let mut alt = false;
+    let mut shift = false;
+
     while !window.should_close() {
+        {
+            let gwindow = window.glfw_window();
+            ctrl = gwindow.get_key(Key::LeftControl) == Action::Press || gwindow.get_key(Key::RightControl) == Action::Press;
+            alt = gwindow.get_key(Key::LeftAlt) == Action::Press || gwindow.get_key(Key::RightAlt) == Action::Press;
+            shift = gwindow.get_key(Key::LeftShift) == Action::Press || gwindow.get_key(Key::RightShift) == Action::Press;
+        }
+
         for mut event in window.events().iter() {
             match event.value {
                 WindowEvent::Scroll(x, y) => {
@@ -28,6 +39,8 @@ fn main() {
             // do not let any events get handled by the engine
             event.inhibited = true;
         }
+
+        println!("ctrl: {}, alt: {}, shift: {}", ctrl, alt, shift);
 
         window.render_with_camera(&mut camera);
     }
