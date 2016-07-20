0c8b8ef7c4e9678b680b12346dc88cef1659d63e
Sunjay Varma <varma.sunjay@gmail.com>
2016-07-20 02:01:18 -0400

    Custom scroll controls

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,34 @@ extern crate glfw;
 extern crate kiss3d;
 extern crate nalgebra as na;
 
+use na::Point3;
 use glfw::WindowEvent;
 use kiss3d::window::Window;
 use kiss3d::light::Light;
+use kiss3d::camera::{Camera, ArcBall};
 
 fn main() {
     let mut window = Window::new("Voxel Editor");
-    let mut c = window.add_cube(0.5, 0.5, 0.5);
+    window.add_cube(1.0, 1.0, 1.0).set_color(1.0, 0.0, 0.0);
 
-    c.set_color(1.0, 0.0, 0.0);
+    let mut camera = ArcBall::new(Point3::new(2f32, 2.0, 2.0), na::origin());
 
-    window.set_light(Light::StickToCamera);
+    window.set_light(Light::Absolute(na::Point3::new(0.0, 10.0, 10.0)));
 
     while !window.should_close() {
         for mut event in window.events().iter() {
             match event.value {
-                WindowEvent::Scroll(x, y) => println!("{} {}", x, y),
+                WindowEvent::Scroll(x, y) => {
+                    let event = WindowEvent::Scroll(x, -y);
+                    camera.handle_event(&window.glfw_window(), &event);
+                },
                 _ => {}
             }
             // do not let any events get handled by the engine
             event.inhibited = true;
         }
 
-        window.render();
+        window.render_with_camera(&mut camera);
     }
 }
 
